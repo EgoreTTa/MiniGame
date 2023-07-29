6186fa106f1cd1a56f9f2402f708e29c5f47d979
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-07-30 01:30:16 +0700

    Create Damage.cs

---
+++ Assets/Damage.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage
+{
+    private BaseMob _owner;
+    private GameObject _parent;
+    private TypeDamage _typeDamage;
+    private List<BaseEffect> _effects;
+
+    public Damage (
+        BaseMob owner, 
+        GameObject parent, 
+        TypeDamage typeDamage, 
+        List<BaseEffect> effects)
+    {
+        _owner = owner;
+        _parent = parent;
+        _typeDamage = typeDamage;
+        _effects = effects;
+    }
+}
+
+public enum TypeDamage
+{
+    Physical,
+    Magical,
+    Clear
+}
