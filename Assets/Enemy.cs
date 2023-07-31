6b25cb89f81ae2f699723c8e90538dc160d7b5f2
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-07-31 22:31:38 +0700

    Update Enemy.cs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,30 +1,57 @@
+using System.Linq;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 public class Enemy : BaseMob
 {
     [SerializeField] private int _score;
-    [SerializeField] private GameObject _targetToAttack;
+    [SerializeField] private BaseMob _targetToAttack;
+    [SerializeField] private BaseItem _targetToPickUp;
 
-    public GameObject TargetToAttack
+    public BaseMob TargetToAttack
     {
         get => _targetToAttack;
         set
         {
-            if (value.activeSelf)
+            if (value.gameObject.activeSelf)
             {
                 _targetToAttack = value;
             }
         }
     }
-    
+
+    public BaseItem TargetToPickUp
+    {
+        get => _targetToPickUp;
+        set
+        {
+            if (value.gameObject.activeSelf)
+            {
+                _targetToPickUp = value;
+            }
+        }
+    }
+
     private void Update()
     {
         if (_targetToAttack != null
             &&
-            _targetToAttack.activeSelf)
+            _targetToAttack.gameObject.activeSelf)
         {
-            MoveToTarget(_targetToAttack);
+            if (_health <= _maxHealth / 100 * 20)
+            {
+                MoveToTarget(_targetToAttack.gameObject);
+                return;
+            }
+
+            Retreat();
+        }
+
+        if (_targetToPickUp != null
+            &&
+            _targetToPickUp.gameObject.activeSelf)
+        {
+            MoveToTarget(_targetToPickUp.gameObject);
         }
     }
 
@@ -36,6 +63,46 @@ public class Enemy : BaseMob
             transform.up,
             Vector3.back);
         Rotate(axis);
+        Walk();
+    }
+
+    private void LookAround()
+    {
+        var casted = Physics2D.CircleCastAll(
+            transform.position,
+            _viewRadius,
+            Vector2.zero);
+        var mobs = casted
+            .Where(x => x.transform.GetComponent<BaseMob>()
+                        &&
+                        x.transform.GetComponent<BaseMob>() != this)
+            .Distinct()
+            .Select(x => x.transform.GetComponent<BaseMob>())
+            .ToArray();
+        if (mobs.Length > 0)
+        {
+            TargetToAttack = mobs.First();
+            return;
+        }
+
+        var items = casted.Where(x => x.transform.GetComponent<BaseItem>())
+            .Distinct()
+            .Select(x => x.transform.GetComponent<BaseItem>())
+            .ToArray();
+        if (items.Length > 0)
+        {
+            TargetToPickUp = items.First();
+        }
+    }
+
+    private void Retreat()
+    {
+        var direction = _targetToAttack.transform.position - transform.position;
+        var axis = Vector3.SignedAngle(
+            direction,
+            transform.up,
+            Vector3.back);
+        Rotate(-axis);
         Walk();
     }
 }
