116b7b4c791d174b80d5c5a246b65cb9bebd1adf
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-05 14:50:10 +0700

    Update Damage.cs

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -3,23 +3,23 @@ using UnityEngine;
 public class Damage
 {
     private BaseMob _owner;
-    private GameObject _parent;
+    private GameObject _damagedBy;
     private TypeDamage _typeDamage;
     private float _countDamage;
 
     public BaseMob Owner => _owner;
-    public GameObject Parent => _parent;
+    public GameObject DamagedBy => _damagedBy;
     public TypeDamage TypeDamage => _typeDamage;
     public float CountDamage => _countDamage;
 
     public Damage(
         BaseMob owner,
-        GameObject parent,
+        GameObject damagedBy,
         TypeDamage typeDamage,
         float countDamage)
     {
         _owner = owner;
-        _parent = parent;
+        _damagedBy = damagedBy;
         _typeDamage = typeDamage;
         _countDamage = countDamage;
     }
