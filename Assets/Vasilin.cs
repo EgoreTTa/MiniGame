dbd46bc6dc0cbe206fcf5c93130e526f4fdcb18f
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-06 21:34:21 +0700

    Update Vasilin.cs

--- Assets/Vasilin.cs
+++ Assets/Vasilin.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Vasilin : BaseItem
 {
+    [SerializeField] private float _changeMaxMoveSpeed;
+    [SerializeField] private float _changeMoveSpeed;
+    [SerializeField] private float _changeTurningSpeed;
+
     public override void PickUp(BaseMob parent)
     {
         parent.Scorer.PickItem(this);
-        parent.MaxMoveSpeed++;
-        parent.MoveSpeed++;
-        parent.TurningSpeed += 15;
+        parent.MaxMoveSpeed += _changeMaxMoveSpeed;
+        parent.MoveSpeed += _changeMoveSpeed;
+        parent.TurningSpeed += _changeTurningSpeed;
     }
 
     public override void PickDown(BaseMob parent)
     {
-        parent.MaxMoveSpeed--;
-        parent.MoveSpeed--;
-        parent.TurningSpeed -= 15;
+        parent.MaxMoveSpeed -= _changeMaxMoveSpeed;
+        parent.MoveSpeed -= _changeMoveSpeed;
+        parent.TurningSpeed -= _changeTurningSpeed;
     }
 }
