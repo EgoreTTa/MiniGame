f891ba0ef36b85582b4ee2a6879364cc36b1aeee
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-09 23:31:47 +0700

    Refactoring

--- Assets/DefaultArmor.cs
+++ Assets/DefaultArmor.cs
@@ -3,41 +3,12 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class DefaultArmor : BaseItem, IEquipment
 {
-    [SerializeField] private TypesEquipment _typeEquipment;
     [SerializeField] private float _changeMaxHealth;
-    [SerializeField] private BaseMob _parent;
-
-    private SpriteRenderer _spriteRenderer;
-    private Rigidbody2D _rigidbody;
-    private Collider2D _collider;
+    [SerializeField] private TypesEquipment _typeEquipment;
 
     public TypesEquipment TypeEquipment => _typeEquipment;
-
-    private void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();
-    }
-
-    public override void PickUp(BaseMob parent)
-    {
-        _parent = parent;
-        _spriteRenderer.enabled = false;
-        _rigidbody.simulated = false;
-        _collider.enabled = false;
-    }
 
     public void Equip() { _parent.MaxHealth += _changeMaxHealth; }
 
     public void Unequip() { _parent.MaxHealth -= _changeMaxHealth; }
-
-    public override void PickDown(BaseMob parent)
-    {
-        transform.position = parent.transform.position;
-        _parent = null;
-        _spriteRenderer.enabled = true;
-        _rigidbody.simulated = true;
-        _collider.enabled = true;
-    }
 }
--- Assets/Scripts/Bases/BaseItem.cs
+++ Assets/Scripts/Bases/BaseItem.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer),
+    typeof(Rigidbody2D),
+    typeof(Collider2D))]
 public abstract class BaseItem : MonoBehaviour
 {
     [SerializeField] protected string _nameItem;
     [SerializeField] protected string _description;
     [SerializeField] protected int _scoreCount;
-    [SerializeField] protected Sprite _spriteItem;
+
+    protected BaseMob _parent;
+    protected SpriteRenderer _spriteRenderer;
+    protected Rigidbody2D _rigidbody;
+    protected Collider2D _collider;
 
     public string NameItem => _nameItem;
     public string Description => _description;
     public int ScoreCount => _scoreCount;
-    public Sprite SpriteItem => _spriteItem;
 
-    public abstract void PickUp(BaseMob parent);
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+    }
 
-    public virtual void PickDown(BaseMob parent) { }
+    public virtual void PickUp(BaseMob parent)
+    {
+        _parent = parent;
+        _spriteRenderer.enabled = false;
+        _rigidbody.simulated = false;
+        _collider.enabled = false;
+    }
+
+    public virtual void PickDown(BaseMob parent)
+    {
+        transform.position = parent.transform.position;
+        _parent = null;
+        _spriteRenderer.enabled = true;
+        _rigidbody.simulated = true;
+        _collider.enabled = true;
+    }
 }
