d7771b5499445b326fcfed2bba8f4f85e481416e
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-14 00:26:34 +0700

    Create BaseElementalEffect.cs

---
+++ Assets/Scripts/ElementalEffects/BaseElementalEffect.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(BaseMob))]
+public abstract class BaseElementalEffect : MonoBehaviour
+{
+    [Flags]
+    public enum TypesElement
+    {
+        ElementalEffect1 = 1,
+        ElementalEffect2 = 2,
+        Effect3 = 4,
+        Effect4 = 8,
+        Effect5 = 16,
+
+        ElementalEffect12 = ElementalEffect1 | ElementalEffect2,
+        Effect13 = ElementalEffect1 | Effect3,
+        Effect14 = ElementalEffect1 | Effect4,
+        Effect15 = ElementalEffect1 | Effect5,
+        Effect23 = ElementalEffect2 | Effect3,
+        Effect24 = ElementalEffect2 | Effect4,
+        Effect25 = ElementalEffect2 | Effect5,
+        Effect34 = Effect3 | Effect4,
+        Effect35 = Effect3 | Effect5,
+        Effect45 = Effect4 | Effect5,
+    }
+
+    protected BaseMob _target;
+    protected BaseMob _owner;
+    [SerializeField] protected TypesElement _typeElement;
+
+    public TypesElement TypeElement => _typeElement;
+
+    public BaseMob Target => _target;
+
+    public BaseMob Owner => _owner;
+
+    private void Awake()
+    {
+        _target = GetComponent<BaseMob>();
+    }
+
+    protected void CombineEffect()
+    {
+        var elementalEffects = GetComponents<BaseElementalEffect>();
+        elementalEffects = elementalEffects.Where(x =>
+                x.TypeElement is TypesElement.ElementalEffect1 or
+                    TypesElement.ElementalEffect2 or
+                    TypesElement.Effect3 or
+                    TypesElement.Effect4 or
+                    TypesElement.Effect5)
+            .ToArray();
+        if (elementalEffects.Length > 1)
+        {
+            var elementalEffect1 = elementalEffects[0];
+            var elementalEffect2 = elementalEffects[1];
+
+            switch (elementalEffect1.TypeElement |
+                    elementalEffect2.TypeElement)
+            {
+                case TypesElement.ElementalEffect12:
+                    Destroy(elementalEffect1);
+                    Destroy(elementalEffect2);
+                    gameObject.AddComponent<ElementalEffect12>();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
