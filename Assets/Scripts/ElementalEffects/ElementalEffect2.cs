28ea03099bbef181665b462ea6ccd4da25c8e660
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-14 00:27:02 +0700

    Create ElementalEffect2.cs

---
+++ Assets/Scripts/ElementalEffects/ElementalEffect2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ElementalEffect2 : BaseElementalEffect
+{
+    [SerializeField] private float _changeMoveSpeed = 1f;
+    [SerializeField] private float _timeOfAction = 10f;
+    private float _timerOfAction;
+
+    private void Start()
+    {
+        _typeElement = TypesElement.ElementalEffect2;
+        CombineEffect();
+        _target.MoveSpeed -= _changeMoveSpeed;
+    }
+
+    private void Update()
+    {
+        if (_timerOfAction < _timeOfAction)
+        {
+            _timerOfAction += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _target.MoveSpeed += _changeMoveSpeed;
+    }
+}
