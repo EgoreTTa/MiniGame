b87c641288b4392cefd22b97823ff7071fe1e879
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-14 01:34:54 +0700

    Update SampleScene.unity

--- Assets/Scripts/ItemDropSystem.cs
+++ Assets/Scripts/ItemDropSystem.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public static class ItemDropSystem
 {
-    private static GameObject[] _items =
-        Resources.LoadAll<GameObject>("");
+    private static readonly GameObject[] Items = Resources.LoadAll<GameObject>("");
 
     public static void Drop(Vector3 position)
     {
         var chance = Random.Range(0, 100);
         if (chance < 100)
         {
-            var randomItem = Random.Range(0, _items.Length);
-            Object.Instantiate(_items[randomItem], position, Quaternion.identity);
+            var randomItem = Random.Range(0, Items.Length);
+            Object.Instantiate(Items[randomItem], position, Quaternion.identity);
         }
     }
 }
