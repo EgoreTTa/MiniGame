5b76eb7664b5a94b3b577b1daa468103a74854a5
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-21 19:54:45 +0700

    Update BaseMob.cs

--- Assets/Scripts/Enemys/BaseMob.cs
+++ Assets/Scripts/Enemys/BaseMob.cs
@@ -179,9 +179,9 @@ public abstract class BaseMob : MonoBehaviour, IHealthSystem
     {
         Health -= damage.TypeDamage switch
         {
-            TypesDamage.Physical => damage.CountHealth / 2,
-            TypesDamage.Magical => damage.CountHealth * 2,
-            TypesDamage.Clear => damage.CountHealth,
+            TypesDamage.Physical => damage.CountDamage / 2,
+            TypesDamage.Magical => damage.CountDamage * 2,
+            TypesDamage.Clear => damage.CountDamage,
             _ => throw new ArgumentOutOfRangeException()
         };
         if (_live is false)
