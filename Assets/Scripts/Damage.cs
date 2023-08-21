016ca7d8bc57736e0f4cb655621577642a0d6ccb
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-21 19:54:52 +0700

    Update Damage.cs

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 public class Damage
 {
     private BaseMob _owner;
-    private GameObject _healthBy;
-    private float _countHealth;
+    private GameObject _damageBy;
+    private float _countDamage;
     private TypesDamage _typeDamage;
 
     public BaseMob Owner => _owner;
-    public GameObject HealthBy => _healthBy;
-    public float CountHealth => _countHealth;
+    public GameObject DamageBy => _damageBy;
+    public float CountDamage => _countDamage;
     public TypesDamage TypeDamage => _typeDamage;
 
     public Damage(
         BaseMob owner,
-        GameObject healthBy,
-        float countHealth,
+        GameObject damageBy,
+        float countDamage,
         TypesDamage typeDamage)
     {
         _owner = owner;
-        _healthBy = healthBy;
-        _countHealth = countHealth;
+        _damageBy = damageBy;
+        _countDamage = countDamage;
         _typeDamage = typeDamage;
     }
 }
