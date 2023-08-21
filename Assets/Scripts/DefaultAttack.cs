de2496ac4dcfa205d6acf19be9fee7742e582827
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-08-22 03:33:37 +0700

    Update DefaultAttack.cs

--- Assets/Scripts/DefaultAttack.cs
+++ Assets/Scripts/DefaultAttack.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(BaseMob))]
 public class DefaultAttack : MonoBehaviour, IAttack
 {
     [SerializeField] private float _timeSwing;
@@ -12,17 +10,20 @@ public class DefaultAttack : MonoBehaviour, IAttack
     private float _timerHitting;
     [SerializeField] private float _timeRecovery;
     private float _timerRecovery;
-    [SerializeField] private float _offsetForAttack;
-    [SerializeField] private float _radiusForAttack;
     [SerializeField] private StatesOfAttack _stateOfAttack;
     private BaseMob _owner;
+    private SpriteRenderer _spriteRenderer;
+    private CircleCollider2D _circleCollider;
     private readonly List<IHealthSystem> _affectedTargets = new();
 
     public StatesOfAttack StateOfAttack => _stateOfAttack;
 
     private void Awake()
     {
-        _owner = GetComponent<BaseMob>();
+        if (GetComponentInParent<BaseMob>() is { } baseMob) _owner = baseMob;
+        else throw new Exception("Default not instance BaseMob");
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _circleCollider = GetComponent<CircleCollider2D>();
     }
 
     public void Attack()
@@ -47,39 +48,14 @@ public class DefaultAttack : MonoBehaviour, IAttack
 
     private void Hit()
     {
-        var triggerPosition =
-            transform.position +
-            _owner.Direction * _offsetForAttack;
+        foreach (var hit in _affectedTargets)
+        {
+            var damage = new Damage(_owner, null, _owner.DamageCount, TypesDamage.Clear);
+            hit.TakeDamage(damage);
+            _affectedTargets.Remove(hit);
 
-        var mobs = GetMobsForRadius(triggerPosition, _radiusForAttack);
-
-        if (mobs.Length > 0)
-            foreach (var hit in mobs)
-                if (_affectedTargets.Contains(hit) is false)
-                {
-                    var damage = new Damage(_owner, null, _owner.DamageCount, TypesDamage.Clear);
-                    hit.TakeDamage(damage);
-                    _affectedTargets.Add(hit);
-
-                    break;
-                }
-    }
-
-    private IHealthSystem[] GetMobsForRadius(Vector3 zero, float radius)
-    {
-        var casted = Physics2D.CircleCastAll(
-            zero,
-            radius,
-            Vector2.zero);
-        var mobs = casted
-            .Where(x =>
-                x.transform.GetComponent<IHealthSystem>() != null
-                &&
-                x.transform.GetComponent<IHealthSystem>() as BaseMob != _owner)
-            .Distinct()
-            .Select(x => x.transform.GetComponent<IHealthSystem>())
-            .ToArray();
-        return mobs;
+            break;
+        }
     }
 
     private void Update()
@@ -97,6 +73,8 @@ public class DefaultAttack : MonoBehaviour, IAttack
             case StatesOfAttack.Swing:
                 if (_timerSwing > _timeSwing)
                 {
+                    _spriteRenderer.enabled = true;
+                    _circleCollider.enabled = true;
                     _timerSwing -= _timeSwing;
                     _stateOfAttack = StatesOfAttack.Hitting;
                     return;
@@ -107,6 +85,8 @@ public class DefaultAttack : MonoBehaviour, IAttack
             case StatesOfAttack.Hitting:
                 if (_timerHitting > _timeHitting)
                 {
+                    _spriteRenderer.enabled = false;
+                    _circleCollider.enabled = false;
                     _timerHitting -= _timeHitting;
                     _affectedTargets.Clear();
                     _stateOfAttack = StatesOfAttack.Recovery;
@@ -138,5 +118,14 @@ public class DefaultAttack : MonoBehaviour, IAttack
     private void Swing()
     {
         if (_timerSwing <= _timeSwing) _timerSwing += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<IHealthSystem>() is { } healthSystem)
+        {
+            if (collider.gameObject != _owner.gameObject)
+                _affectedTargets.Add(healthSystem);
+        }
     }
 }
