e991d14c1fbe1910a34fd5e2e9f1c910cfa6fb17
DarkTinker23 <dav23330@gmail.com>
2023-09-02 19:52:44 +0300

    исправленная табуляция

--- Assets/Scripts/Items/Equipments/FireArmor.cs
+++ Assets/Scripts/Items/Equipments/FireArmor.cs
@@ -8,24 +8,26 @@ namespace Assets.Scripts.Items.Equipments
     using NoMonoBehaviour;
 
     [DisallowMultipleComponent]
-
     public class FireArmor : BaseItem, IEquipment
     {
         [SerializeField] private float _changeMaxHealth;
         [SerializeField] private float _changeRegeneration;
-        [SerializeField] private GameObject FireCircke;
+        [SerializeField] private GameObject _fireCircke;
         [SerializeField] private TypesEquipment _typeEquipment;
         public TypesEquipment TypeEquipment => _typeEquipment;
+
         private void Regen()
         {
             var health = new Health(_owner, gameObject, _changeRegeneration);
             (_owner as IHealthSystem).TakeHealth(health);
         }
+
         public void Equip()
         {
             _owner.MaxHealth += _changeMaxHealth;
-            InvokeRepeating("Regen", 0f, 1f);
+            InvokeRepeating(nameof(Regen), 0f, 1f);
         }
+
         public void Unequip()
         {
             _owner.MaxHealth -= _changeMaxHealth;
--- Assets/Scripts/Items/Equipments/FireBoot.cs
+++ Assets/Scripts/Items/Equipments/FireBoot.cs
@@ -1,7 +1,5 @@
-
 namespace Assets.Scripts.Items.Equipments
 {
-
     using Enums;
     using Interfaces;
     using UnityEngine;
@@ -10,29 +8,30 @@ namespace Assets.Scripts.Items.Equipments
     using NoMonoBehaviour;
 
     [DisallowMultipleComponent]
-
     public class FireBoot : BaseItem, IEquipment
     {
         [SerializeField] private float _changeMaxHealth;
         [SerializeField] private float _changeRegeneration;
-        [SerializeField] private GameObject FireCircke;
+        [SerializeField] private GameObject _fireCircke;
         [SerializeField] private TypesEquipment _typeEquipment;
         public TypesEquipment TypeEquipment => _typeEquipment;
+
         private void Regen()
         {
             var health = new Health(_owner, gameObject, _changeRegeneration);
             (_owner as IHealthSystem).TakeHealth(health);
         }
+
         public void Equip()
         {
             _owner.MaxHealth += _changeMaxHealth;
-            InvokeRepeating("Regen", 0f, 1f);
+            InvokeRepeating(nameof(Regen), 0f, 1f);
         }
+
         public void Unequip()
         {
             _owner.MaxHealth -= _changeMaxHealth;
             CancelInvoke(nameof(Regen));
         }
-        
     }
 }
