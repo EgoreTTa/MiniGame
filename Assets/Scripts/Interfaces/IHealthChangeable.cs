a405efbe2a5d100d3ab8f688ca85604044388bfa
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-10-22 12:49:11 +0500

    Update IHealthChangeable.cs

--- Assets/Scripts/Interfaces/IHealthChangeable.cs
+++ Assets/Scripts/Interfaces/IHealthChangeable.cs
@@ -3,5 +3,7 @@ namespace Assets.Scripts.Interfaces
     public interface IHealthChangeable
     {
         public void UpdateHealthSystem(BaseHealthSystem healthSystem);
+        public void TakeHealth(BaseHealthSystem healthSystem);
+        public void TakeDamage(BaseHealthSystem healthSystem);
     }
 }
