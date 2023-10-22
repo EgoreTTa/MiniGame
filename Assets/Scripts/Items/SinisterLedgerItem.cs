134a3a1bb6323f003bb2c053184c3e1ba92ff2b3
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2023-10-22 12:47:45 +0500

    Create SinisterLedgerItem.cs

---
+++ Assets/Scripts/Items/SinisterLedgerItem.cs
@@ -0,0 +1,51 @@
+namespace Assets.Scripts.Items
+{
+    using Interfaces;
+    using JetBrains.Annotations;
+    using Mobs;
+    using Mobs.Player;
+    using UnityEngine;
+
+    [UsedImplicitly]
+    [DisallowMultipleComponent]
+    public class SinisterLedger : BaseItem, IKillerMob, IHealthChangeable
+    {
+        [SerializeField] private float _powerDamagePercent;
+        [SerializeField] private float _incrementDamagePercent;
+        [SerializeField] private float _maxDamagePercent;
+
+        public override void PickUp(Player owner)
+        {
+            base.PickUp(owner);
+            owner.Subscribe(this);
+            owner.HealthSystem.Subscribe(this);
+        }
+
+        public override void PickDown(Player owner)
+        {
+            base.PickDown(owner);
+            owner.Unsubscribe(this);
+            owner.HealthSystem.Unsubscribe(this);
+            owner.Inventory.Take(this);
+        }
+
+        public void Killer(BaseMob whomKilled)
+        {
+            if (_powerDamagePercent < _maxDamagePercent)
+            {
+                _owner.AttackSystem.DamagePercent += _incrementDamagePercent;
+                _powerDamagePercent += _incrementDamagePercent;
+            }
+        }
+
+        public void UpdateHealthSystem(BaseHealthSystem healthSystem) { }
+
+        public void TakeHealth(BaseHealthSystem healthSystem) { }
+
+        public void TakeDamage(BaseHealthSystem healthSystem)
+        {
+            _owner.AttackSystem.DamagePercent -= _powerDamagePercent;
+            _powerDamagePercent = default;
+        }
+    }
+}
