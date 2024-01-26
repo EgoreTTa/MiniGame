b3c347a2998b82468a736429711e7d71b0899f2d
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2024-01-26 05:08:25 +0500

    Update BerserkerHelm.cs

--- Assets/Scripts/Items/BerserkerHelm.cs
+++ Assets/Scripts/Items/BerserkerHelm.cs
@@ -2,12 +2,13 @@ namespace Items
 {
     using Interfaces;
     using JetBrains.Annotations;
+    using Mobs;
     using Mobs.Player;
     using UnityEngine;
 
     [UsedImplicitly]
     [DisallowMultipleComponent]
-    public class BerserkerHelm : BaseItem, IHealthChangeable
+    public class BerserkerHelm : BaseItem, ITakeDamage, ITakeHealth
     {
         private bool _isActive;
         [SerializeField] private float _damagePercent;
@@ -16,45 +17,51 @@ namespace Items
         public override void PickUp(Player owner)
         {
             base.PickUp(owner);
-            owner.HealthSystem.Subscribe(this);
-            UpdateHealthSystem(owner.HealthSystem);
+            owner.HealthSystem.Subscribe(this as ITakeHealth);
+            owner.HealthSystem.Subscribe(this as ITakeDamage);
+            CheckActivationOptions(owner.HealthSystem);
         }
 
         public override void PickDown(Player owner)
         {
             base.PickDown(owner);
-            owner.HealthSystem.Unsubscribe(this);
+            owner.HealthSystem.Unsubscribe(this as ITakeHealth);
+            owner.HealthSystem.Unsubscribe(this as ITakeDamage);
             owner.Inventory.Take(this);
         }
 
         private void Activate()
         {
-            _owner.AttackSystem.DamagePercent += _damagePercent;
+            _owner.Attribute.AttackRelativeDamage += _damagePercent;
             _isActive = true;
         }
 
         private void Deactivate()
         {
-            _owner.AttackSystem.DamagePercent -= _damagePercent;
+            _owner.Attribute.AttackRelativeDamage -= _damagePercent;
             _isActive = false;
         }
 
-        public void UpdateHealthSystem(BaseHealthSystem healthSystem)
+        private void CheckActivationOptions(BaseHealthSystem healthSystem)
         {
             if (healthSystem.Health / healthSystem.MaxHealth <= _healthThreshold)
             {
-                if (_isActive is false)
-                    Activate();
+                if (_isActive is false) Activate();
             }
             else
             {
-                if (_isActive is true)
-                    Deactivate();
+                if (_isActive is true) Deactivate();
             }
         }
 
-        public void TakeHealth(BaseHealthSystem healthSystem) { }
+        public void TakeHealth(BaseHealthSystem healthSystem)
+        {
+            CheckActivationOptions(healthSystem);
+        }
 
-        public void TakeDamage(BaseHealthSystem healthSystem) { }
+        public void TakeDamage(BaseHealthSystem healthSystem)
+        {
+            CheckActivationOptions(healthSystem);
+        }
     }
 }
